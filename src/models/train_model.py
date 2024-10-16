175c09f10621084881afb136c54b794b782271e9
NembotJules <nembotjulesgithub@gmail.com>
2024-10-16 02:45:03 +0100

    Started implementing the training logic

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,11 +1,33 @@
+import mlflow
+import pandas as pd
 
 
-def train_model(X_train, y_train): 
+best_run_id = '06c745047aa74cc3abdf15de9d58704d'
+best_model_name = 'XGBR2'
 
-    # Load the model from S3 bucket (Model Registry)
+mlflow.set_tracking_uri('http://35.153.184.244:5000/')
+mlflow.set_experiment('air-quality-prediction')
+
+def train_model(X, y): 
+
+    logged_model = f'runs:/{best_run_id}/{best_model_name}'
+
+    # Load model as a PyFuncModel.
+    XGBR2 = mlflow.xgboost.load_model(logged_model)
 
     # Train the model on the full dataset
+    XGBR2.fit(X, y) 
 
-    # Save the model to S3 bucket (Model Registry)
+    # Log the model with mlflow
+    mlflow.xgboost.log_model(XGBR2, artifact_path = best_model_name)
 
-    
+    return XGBR2
+
+
+if __name__ == "__main__": 
+
+    X = pd.read_csv('../../data/train_test_data/X.csv')
+    y = pd.read_csv('../../data/train_test_data/y.csv')
+
+    train_model(X, y)
+
