3a901270295ef886bfade57be648b80dcf8cb033
NembotJules <nembotjulesgithub@gmail.com>
2024-10-16 16:48:11 +0100

    Remove train_test_split import from sklearn

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.pipeline import FunctionTransformer
 from sklearn.pipeline import Pipeline
