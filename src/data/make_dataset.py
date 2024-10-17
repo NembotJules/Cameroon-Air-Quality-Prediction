fab78892d8daa02a8d1d09f237aa413b5529cf16
NembotJules <nembotjulesgithub@gmail.com>
2024-10-17 19:05:44 +0100

    Saving is now on S3

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,10 +1,16 @@
 import pandas as pd
 import numpy as np
+import mlflow
+import yaml
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.pipeline import FunctionTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 
+default_config_name = "../../config/default.yaml"
+
+with open(default_config_name, "r") as file: 
+    default_config = yaml.safe_load(file)
 
 
 
@@ -79,9 +85,9 @@ def preprocessor_transform(X,numeric_features, categorical_features):
 
 def preprocess_data(df):
 
-    if 'pm2_5' in df.columns: 
-        X = clean_data(df).drop('pm2_5', axis = 1)
-        y = clean_data(df)['pm2_5']
+    if default_config["target_column"] in df.columns: 
+        X = clean_data(df).drop(default_config["target_column"], axis = 1)
+        y = clean_data(df)[default_config["target_column"]]
 
     else: 
         X = clean_data(df)
@@ -95,23 +101,46 @@ def preprocess_data(df):
     return X, y
 
 
-    
-    
-
-
-def save_data(df, file_path, dataset_name):
+def save_data(df, file_path):
 
     X, y = preprocess_data(df)
-    X.to_csv(file_path + f"{dataset_name}_X.csv", index=False)
 
-    if y is not None: 
-        y.to_csv(file_path + f"{dataset_name}_y.csv", index = False)
+    if X.empty: 
+        print(f"Warning: X is empty. No data will be saved.")
+        return 
+
+    try: 
+    
+        
+        if y is not None: 
+            if len(X) != len(y): 
+                print(f"Warning: X and y have different lengths. No data will be saved.")
+                return 
+            y.to_csv(default_config["data"]["preprocessed_train_target_path"], index = False)
+            print(" Train Target data saved successfully.")
+
+            X.to_csv(default_config["data"]["preprocessed_train_data_path"], index=False)
+            print("Training Feature data saved successfully.")
+            print(X.shape)
+
+        else: 
+            X.to_csv(default_config["data"]["preprocessed_test_data_path"], index=False)
+            print("Test Feature data saved successfully.")
+            print(X.shape)
+
+
+
+    except Exception as e: 
+        print(f"Error saving data: {e}")
+
 
 if __name__ == "__main__":
-    df = load_data('../../data/train_test_data/train.csv')
-    test_data = load_data('../../data/train_test_data/test.csv')
-    save_data(df, '../../data/train_test_data/preprocessed_data/', 'train') 
-    save_data(test_data, '../../data/train_test_data/preprocessed_data/', 'test') 
+
+
+    df = load_data(default_config["data"]["raw_train_data_path"])
+    test_data = load_data(default_config["data"]["raw_test_data_path"])
+    save_data(df, default_config["data"]["preprocessed_train_data_path"]) 
+    save_data(test_data, default_config["data"]["preprocessed_test_data_path"]) 
 
 
 
