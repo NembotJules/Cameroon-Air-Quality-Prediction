f960188540cb67149f1d0e930bec322122f3a8c4
NembotJules <nembotjulesgithub@gmail.com>
2024-10-18 21:03:13 +0100

    Updates...

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,13 +1,17 @@
 import pandas as pd
 import numpy as np
-import mlflow
+import os
 import yaml
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.pipeline import FunctionTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 
-default_config_name = "../../config/default.yaml"
+# Get the directory of the current file
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the config file
+default_config_name = os.path.join(current_dir, '..', '..', 'config', 'default.yaml')
 
 with open(default_config_name, "r") as file: 
     default_config = yaml.safe_load(file)
@@ -78,12 +82,15 @@ def preprocessor_transform(X,numeric_features, categorical_features):
     X_transformed = preprocessor.fit_transform(X)
     print(len(X_transformed))
     X = to_dataframe(X_transformed, X.columns)
+    
 
     return X
 
 
 
 def preprocess_data(df):
+
+    print(f"The shape of the data at the beginning of preprocess_data {df.shape}")
 
     if default_config["target_column"] in df.columns: 
         X = clean_data(df).drop(default_config["target_column"], axis = 1)
@@ -93,10 +100,14 @@ def preprocess_data(df):
         X = clean_data(df)
         y = None
 
+    print(f"The shape of the data after removing the id and the target column {df.shape}")
+
     
     numeric_features, categorical_features = create_categorical_and_numeric_features(X)
 
     X = preprocessor_transform(X, numeric_features, categorical_features)
+
+    print(f"The shape of the data after the preprocessor_transform call {df.shape}")
 
     return X, y
 
@@ -116,15 +127,19 @@ def save_data(df, file_path):
             if len(X) != len(y): 
                 print(f"Warning: X and y have different lengths. No data will be saved.")
                 return 
-            y.to_csv(default_config["data"]["preprocessed_train_target_path"], index = False)
+           # y.to_csv(default_config["data"]["preprocessed_train_target_path"], index = False)
+            y.to_csv('../../data/train_test_data/preprocessed_data/train_y.csv', index = False)
             print(" Train Target data saved successfully.")
+            print(y.shape)
 
-            X.to_csv(default_config["data"]["preprocessed_train_data_path"], index=False)
+            #X.to_csv(default_config["data"]["preprocessed_train_data_path"], index=False)
+            X.to_csv('../../data/train_test_data/preprocessed_data/train_X.csv', index = False)
             print("Training Feature data saved successfully.")
             print(X.shape)
 
         else: 
-            X.to_csv(default_config["data"]["preprocessed_test_data_path"], index=False)
+            #X.to_csv(default_config["data"]["preprocessed_test_data_path"], index=False)
+            X.to_csv('../../data/train_test_data/preprocessed_data/test_X.csv', index = False)
             print("Test Feature data saved successfully.")
             print(X.shape)
 
@@ -137,10 +152,10 @@ def save_data(df, file_path):
 if __name__ == "__main__":
 
 
-    df = load_data(default_config["data"]["raw_train_data_path"])
-    test_data = load_data(default_config["data"]["raw_test_data_path"])
-    save_data(df, default_config["data"]["preprocessed_train_data_path"]) 
-    save_data(test_data, default_config["data"]["preprocessed_test_data_path"]) 
+    df = load_data('../../data/train_test_data/raw_data/train.csv')
+    test_data = load_data('../../data/train_test_data/raw_data/test.csv')
+    save_data(df, '../../data/train_test_data/preprocessed_data/train_X.csv') 
+    save_data(test_data, '../../data/train_test_data/preprocessed_data/test_X.csv') 
 
 
 
