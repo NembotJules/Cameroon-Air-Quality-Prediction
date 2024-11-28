65da6fb4ec501d5173f44eb1b8c320d831a2c7ee
NembotJules <nembotjulesgithub@gmail.com>
2024-11-28 11:03:57 +0100

    Updates...

--- src/data/test.py
+++ src/data/test.py
@@ -5,4 +5,10 @@ def greatings():
     print("Hello World")
 
 if __name__ == "__main__": 
-    greatings()
+    greatings().from_source(
+        source="https://github.com/NembotJules/Cameroon-Air-Quality-Prediction.git", 
+        entrypoint="src/data/test.py:greatings"
+    ).deploy(
+        name="my-first-deployment", 
+        work_pool_name="Managed-Pool", 
+    )
