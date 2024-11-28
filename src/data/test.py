1362cb96ae0f171590446d75db44afc400ae9e3b
NembotJules <nembotjulesgithub@gmail.com>
2024-11-28 18:08:07 +0100

    Created the short path

--- src/data/test.py
+++ src/data/test.py
@@ -1,42 +1,4 @@
-from prefect import flow, task
-from prefect.runner.storage import GitRepository
-from prefect_github.repository import GitHubRepository
-from prefect_github import GitHubCredentials
+from prefect_aws.s3 import S3Bucket
 
-@flow(name="Test flow")
-def greatings(): 
-    print("Hello World")
-
-
-    
-
-if __name__ == "__main__": 
-
-    
-    greatings.from_source(
-        
-         source=GitRepository(
-            url="https://github.com/NembotJules/Cameroon-Air-Quality-Prediction.git",
-            branch="dev",
-            credentials=GitHubCredentials.load("git-credentials")
-            ),
-        entrypoint = "src/data/test.py:greatings"
-    ).deploy(
-        name="test-pipeline-2", 
-         work_pool_name="Managed-Pool", 
-     )
-# #     greatings.from_source(
-# #         source=GitRepository(
-# #         url="https://github.com/NembotJules/Cameroon-Air-Quality-Prediction.git",
-# #         branch="dev",
-# #         credentials={
-# #             "access_token": Secret.load("github-access-token")
-# #         }
-# #     )).deploy(
-# #         name="my-first-deployment", 
-# #         work_pool_name="Managed-Pool", 
-# #     )
-#
-
-
-
+s3_bucket_block = S3Bucket.load("cameroon-air-quality-bucket")
+print(s3_bucket_block)
