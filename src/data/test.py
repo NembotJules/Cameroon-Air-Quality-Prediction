28f8b0625d18378c087a66322d991d279c4b313b
NembotJules <nembotjulesgithub@gmail.com>
2024-11-28 11:38:06 +0100

    Updates...

--- src/data/test.py
+++ src/data/test.py
@@ -1,11 +1,32 @@
 from prefect import flow, task
+from prefect.runner.storage import GitRepository
+from prefect_github.repository import GitHubRepository
+from prefect.blocks.system import Secret
 
 @flow(name="Test flow")
 def greatings(): 
     print("Hello World")
+    
 
 if __name__ == "__main__": 
-    greatings.deploy(
+    greatings.from_source(
+        source = GitHubRepository.load("cameroon-air-quality-prediction-project-repo")
+    ).deploy(
         name="my-first-deployment", 
-        work_pool_name="Managed-Pool", 
-    )
+         work_pool_name="Managed-Pool", 
+     )
+#     greatings.from_source(
+#         source=GitRepository(
+#         url="https://github.com/NembotJules/Cameroon-Air-Quality-Prediction.git",
+#         branch="dev",
+#         credentials={
+#             "access_token": Secret.load("github-access-token")
+#         }
+#     )).deploy(
+#         name="my-first-deployment", 
+#         work_pool_name="Managed-Pool", 
+#     )
+
+
+
+print(GitHubRepository.load("cameroon-air-quality-prediction-project-repo"))
